5ea0e9a0ca11a6420900272a82325f0e070df741
mza-codes <muzofficial90@gmail.com>
2022-11-14 19:27:21 +0000

    Added CSS Scrolling

--- src/Components/HorizontalRow/HorizRow.jsx
+++ src/Components/HorizontalRow/HorizRow.jsx
@@ -1,33 +1,58 @@
 import { w500 } from '../../Constants/Constants';
 import { useState } from 'react';
 import LazyImage from '../LazyImage';
-import styled from 'styled-components';
 import './HorizRow.scss';
 
 const HorizRow = ({ data, title, ...props }) => {
     // data = [];
     const [scrollValue, setScrollValue] = useState(0);
-    // const value = "0%"
-    const ScrollRow = styled.div`
-        transform: translateX(-${scrollValue}%);
-    `
+
+    const handleScroll = (param) => {
+        switch (param) {
+            case "next":
+                console.log("NEXT");
+                if (scrollValue > 1500) {
+                    setScrollValue(2300);
+                    console.log("limit reached", scrollValue);
+                    return;
+                };
+                setScrollValue((curr) => (curr + 500));
+                break;
+            case "prev":
+                console.log("PREVI");
+                if (scrollValue < 0) {
+                    setScrollValue(0);
+                    console.log("scroll value negative", scrollValue);
+                    return;
+                };
+                setScrollValue((curr) => (curr - 500));
+                break;
+            default:
+                break;
+        };
+    };
+
     return (
         <div>
             {data.length ? <>
                 <h3 className='px-4 text-white text-2xl font-righteous pointer-events-none capitalize'>
                     {title || "Loading.."}
                 </h3>
-                <ScrollRow className="horizRow">
+                <div className="horizRow">
                     <div className="scrollButtons">
-                        <button onClick={() => setScrollValue((curr) => (curr + 10))} >&gt;</button>
-                        <button onClick={() => setScrollValue((curr) => (curr - 10))}>&lt;</button>
+                        <button onClick={() => handleScroll("next")} onMouseEnter={e => handleScroll("next")} >
+                            <i className="ri-arrow-right-s-line"></i>
+                        </button>
+                        <button onClick={() => handleScroll("prev")} onMouseEnter={e => handleScroll("prev")} >
+                            <i className="ri-arrow-left-s-line"></i>
+                        </button>
                     </div>
                     {data.map((item, i) => (
-                        <div key={i} className="poster">
+                        <div key={i} className="poster" style={{ translate: `-${scrollValue}px` }}>
                             <LazyImage url={w500 + (item?.poster_path || item?.backdrop_path)} />
                         </div>
                     ))}
-                </ScrollRow>
+                </div>
             </> : <>
                 <div className='loadText'></div>
                 <div className='rowSkeleton'>
