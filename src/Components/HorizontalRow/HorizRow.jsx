9a411d055fc1d3c9b0cbe8ee5315fe9f71f6a44c
mza-codes <muzofficial90@gmail.com>
2022-11-14 15:12:46 +0000

    Added StyledComponents

--- src/Components/HorizontalRow/HorizRow.jsx
+++ src/Components/HorizontalRow/HorizRow.jsx
@@ -1,28 +1,39 @@
 import { w500 } from '../../Constants/Constants';
+import { useState } from 'react';
 import LazyImage from '../LazyImage';
+import styled from 'styled-components';
 import './HorizRow.scss';
 
 const HorizRow = ({ data, title, ...props }) => {
     // data = [];
+    const [scrollValue, setScrollValue] = useState(0);
+    // const value = "0%"
+    const ScrollRow = styled.div`
+        transform: translateX(-${scrollValue}%);
+    `
     return (
         <div>
             {data.length ? <>
                 <h3 className='px-4 text-white text-2xl font-righteous pointer-events-none capitalize'>
                     {title || "Loading.."}
                 </h3>
-                <div className="horizRow">
+                <ScrollRow className="horizRow">
+                    <div className="scrollButtons">
+                        <button onClick={() => setScrollValue((curr) => (curr + 10))} >&gt;</button>
+                        <button onClick={() => setScrollValue((curr) => (curr - 10))}>&lt;</button>
+                    </div>
                     {data.map((item, i) => (
                         <div key={i} className="poster">
                             <LazyImage url={w500 + (item?.poster_path || item?.backdrop_path)} />
                         </div>
                     ))}
-                </div>
+                </ScrollRow>
             </> : <>
                 <div className='loadText'></div>
                 <div className='rowSkeleton'>
                     <div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div />
                 </div>
-            </> }
+            </>}
         </div>
     )
 }
