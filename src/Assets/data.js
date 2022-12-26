d82bf5e34fcf528e1677b9fa1c85320f6b4d064d
mza-codes <muzofficial90@gmail.com>
2022-12-26 12:53:18 +0000

    Localized Data for Banner, Removed Unused Fonts

---
+++ src/Assets/data.js
@@ -0,0 +1,439 @@
+const movies = [
+    {
+        "adult": false,
+        "backdrop_path": "/lsN1wAbqCvUPKEhkEI9pQSSiTjU.jpg",
+        "id": 661374,
+        "title": "Glass Onion: A Knives Out Mystery",
+        "original_language": "en",
+        "original_title": "Glass Onion: A Knives Out Mystery",
+        "overview": "World-famous detective Benoit Blanc heads to Greece to peel back the layers of a mystery surrounding a tech billionaire and his eclectic crew of friends.",
+        "poster_path": "/vDGr1YdrlfbU9wxTOdpf3zChmv9.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            9648,
+            53,
+            35
+        ],
+        "popularity": 460.888,
+        "release_date": "2022-11-23",
+        "video": false,
+        "vote_average": 7.277,
+        "vote_count": 705
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/laaEVHDFiA57tP7NbWWKbPcVB6q.jpg",
+        "id": 106541,
+        "name": "The Witcher: Blood Origin",
+        "original_language": "en",
+        "original_name": "The Witcher: Blood Origin",
+        "overview": "More than a thousand years before the world of The Witcher, seven outcasts in the elven world unite in a blood quest against an unstoppable power.",
+        "poster_path": "/vpfJK9F0UJNcAIIeC42oJyKMnZQ.jpg",
+        "media_type": "tv",
+        "genre_ids": [
+            10759,
+            10765
+        ],
+        "popularity": 125.279,
+        "first_air_date": "2022-12-25",
+        "vote_average": 5.8,
+        "vote_count": 37,
+        "origin_country": [
+            "US"
+        ]
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg",
+        "id": 76600,
+        "title": "Avatar: The Way of Water",
+        "original_language": "en",
+        "original_title": "Avatar: The Way of Water",
+        "overview": "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+        "poster_path": "/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            878,
+            12,
+            14,
+            28
+        ],
+        "popularity": 11276.345,
+        "release_date": "2022-12-14",
+        "video": false,
+        "vote_average": 8.054,
+        "vote_count": 1231
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/5wDBVictj4wUYZ31gR5WzCM9dLD.jpg",
+        "id": 877269,
+        "title": "Strange World",
+        "original_language": "en",
+        "original_title": "Strange World",
+        "overview": "A journey deep into an uncharted and treacherous land, where fantastical creatures await the legendary Clades—a family of explorers whose differences threaten to topple their latest, and by far most crucial, mission.",
+        "poster_path": "/mPywkW1dfDer6GxxTvjwOTsAVud.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            16,
+            878,
+            12,
+            10751
+        ],
+        "popularity": 457.288,
+        "release_date": "2022-11-23",
+        "video": false,
+        "vote_average": 6.299,
+        "vote_count": 167
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/nWs0auTqn2UaFGfTKtUE5tlTeBu.jpg",
+        "id": 668482,
+        "title": "Roald Dahl's Matilda the Musical",
+        "original_language": "en",
+        "original_title": "Roald Dahl's Matilda the Musical",
+        "overview": "An extraordinary young girl discovers her superpower and summons the remarkable courage, against all odds, to help others change their stories, whilst also taking charge of her own destiny. Standing up for what's right, she's met with miraculous results.",
+        "poster_path": "/ga8R3OiOMMgSvZ4cOj8x7prUNYZ.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            10751,
+            35,
+            14
+        ],
+        "popularity": 55.646,
+        "release_date": "2022-11-25",
+        "video": false,
+        "vote_average": 8.2,
+        "vote_count": 33
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/bKxiLRPVWe2nZXCzt6JPr5HNWYm.jpg",
+        "id": 110316,
+        "name": "Alice in Borderland",
+        "original_language": "ja",
+        "original_name": "今際の国のアリス",
+        "overview": "With his two friends, a video-game-obsessed young man finds himself in a strange version of Tokyo where they must compete in dangerous games to win.",
+        "poster_path": "/20mOwAAPwZ1vLQkw0fvuQHiG7bO.jpg",
+        "media_type": "tv",
+        "genre_ids": [
+            18,
+            9648,
+            10759,
+            10765
+        ],
+        "popularity": 624.144,
+        "first_air_date": "2020-12-10",
+        "vote_average": 8.154,
+        "vote_count": 1016,
+        "origin_country": [
+            "JP"
+        ]
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/g9Kb3RaLjsybI1jpqHQ3QZTCYpB.jpg",
+        "id": 899112,
+        "title": "Violent Night",
+        "original_language": "en",
+        "original_title": "Violent Night",
+        "overview": "When a team of mercenaries breaks into a wealthy family compound on Christmas Eve, taking everyone inside hostage, the team isn’t prepared for a surprise combatant: Santa Claus is on the grounds, and he’s about to show why this Nick is no saint.",
+        "poster_path": "/1XSYOP0JjjyMz1irihvWywro82r.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            28,
+            35,
+            80,
+            53
+        ],
+        "popularity": 2485.161,
+        "release_date": "2022-11-30",
+        "video": false,
+        "vote_average": 7.681,
+        "vote_count": 442
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/iHSwvRVsRyxpX7FE7GbviaDvgGZ.jpg",
+        "id": 119051,
+        "name": "Wednesday",
+        "original_language": "en",
+        "original_name": "Wednesday",
+        "overview": "Wednesday Addams is sent to Nevermore Academy, a bizarre boarding school where she attempts to master her psychic powers, stop a monstrous killing spree of the town citizens, and solve the supernatural mystery that affected her family 25 years ago — all while navigating her new relationships.",
+        "poster_path": "/9PFonBhy4cQy7Jz20NpMygczOkv.jpg",
+        "media_type": "tv",
+        "genre_ids": [
+            10765,
+            9648,
+            35
+        ],
+        "popularity": 4118.805,
+        "first_air_date": "2022-11-23",
+        "vote_average": 8.759,
+        "vote_count": 3852,
+        "origin_country": [
+            "US"
+        ]
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/6ovk8nrrSmN1ieT14zBAxcHbMU7.jpg",
+        "id": 73375,
+        "name": "Tom Clancy's Jack Ryan",
+        "original_language": "en",
+        "original_name": "Tom Clancy's Jack Ryan",
+        "overview": "When CIA analyst Jack Ryan stumbles upon a suspicious series of bank transfers his search for answers pulls him from the safety of his desk job and catapults him into a deadly game of cat and mouse throughout Europe and the Middle East, with a rising terrorist figurehead preparing for a massive attack against the US and her allies.",
+        "poster_path": "/z8yXhmNmc54TsMK2Ig4V4SHdkOX.jpg",
+        "media_type": "tv",
+        "genre_ids": [
+            10759,
+            18,
+            10768
+        ],
+        "popularity": 232.721,
+        "first_air_date": "2018-08-30",
+        "vote_average": 7.681,
+        "vote_count": 956,
+        "origin_country": [
+            "US"
+        ]
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/usXjm3T1iOUl4CZ3mCnFlWR8bi9.jpg",
+        "id": 19995,
+        "title": "Avatar",
+        "original_language": "en",
+        "original_title": "Avatar",
+        "overview": "In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization.",
+        "poster_path": "/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            28,
+            12,
+            14,
+            878
+        ],
+        "popularity": 1544.259,
+        "release_date": "2009-12-15",
+        "video": false,
+        "vote_average": 7.548,
+        "vote_count": 27179
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/AaV1YIdWKnjAIAOe8UUKBFm327v.jpg",
+        "id": 361743,
+        "title": "Top Gun: Maverick",
+        "original_language": "en",
+        "original_title": "Top Gun: Maverick",
+        "overview": "After more than thirty years of service as one of the Navy’s top aviators, and dodging the advancement in rank that would ground him, Pete “Maverick” Mitchell finds himself training a detachment of TOP GUN graduates for a specialized mission the likes of which no living pilot has ever seen.",
+        "poster_path": "/62HCnUTziyWcpDaBO2i1DX17ljH.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            28,
+            18
+        ],
+        "popularity": 707.184,
+        "release_date": "2022-05-24",
+        "video": false,
+        "vote_average": 8.346,
+        "vote_count": 5186
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/9Md4CqzUGDtK5oEkRRvozLkGc9d.jpg",
+        "id": 674324,
+        "title": "The Banshees of Inisherin",
+        "original_language": "en",
+        "original_title": "The Banshees of Inisherin",
+        "overview": "Two lifelong friends find themselves at an impasse when one abruptly ends their relationship, with alarming consequences for both of them.",
+        "poster_path": "/4yFG6cSPaCaPhyJ1vtGOtMD1lgh.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            18,
+            35
+        ],
+        "popularity": 343.598,
+        "release_date": "2022-10-21",
+        "video": false,
+        "vote_average": 7.6,
+        "vote_count": 212
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/jG8mKDxe0LIDFBPB8uCeYGSBWCH.jpg",
+        "id": 153496,
+        "name": "Reborn Rich",
+        "original_language": "ko",
+        "original_name": "재벌집 막내아들",
+        "overview": "After ten years, a loyal employee is framed for embezzlement, then murdered by his employers, only to be reborn as their youngest son, with a yearn for revenge guiding his hostile takeover. based on a hit novel.",
+        "poster_path": "/ioywelRYOfNJ5w8aNQ5ttJo9dk1.jpg",
+        "media_type": "tv",
+        "genre_ids": [
+            18,
+            10765
+        ],
+        "popularity": 231.259,
+        "first_air_date": "2022-11-18",
+        "vote_average": 8.5,
+        "vote_count": 22,
+        "origin_country": [
+            "KR"
+        ]
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+        "id": 436270,
+        "title": "Black Adam",
+        "original_language": "en",
+        "original_title": "Black Adam",
+        "overview": "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+        "poster_path": "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            28,
+            14,
+            878
+        ],
+        "popularity": 3703.659,
+        "release_date": "2022-10-19",
+        "video": false,
+        "vote_average": 7.2,
+        "vote_count": 3257
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/4HWAQu28e2yaWrtupFPGFkdNU7V.jpg",
+        "id": 546554,
+        "title": "Knives Out",
+        "original_language": "en",
+        "original_title": "Knives Out",
+        "overview": "When renowned crime novelist Harlan Thrombey is found dead at his estate just after his 85th birthday, the inquisitive and debonair Detective Benoit Blanc is mysteriously enlisted to investigate. From Harlan's dysfunctional family to his devoted staff, Blanc sifts through a web of red herrings and self-serving lies to uncover the truth behind Harlan's untimely death.",
+        "poster_path": "/pThyQovXQrw2m0s9x82twj48Jq4.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            35,
+            80,
+            9648
+        ],
+        "popularity": 217.536,
+        "release_date": "2019-11-27",
+        "video": false,
+        "vote_average": 7.856,
+        "vote_count": 9743
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/atmII0hn3iQe3IWMBmIb3cc8EJZ.jpg",
+        "id": 791177,
+        "title": "Bones and All",
+        "original_language": "en",
+        "original_title": "Bones and All",
+        "overview": "Abandoned by her father, a young woman embarks on a thousand-mile odyssey through the backroads of America where she meets a disenfranchised drifter. But despite their best efforts, all roads lead back to their terrifying pasts and to a final stand that will determine whether their love can survive their otherness.",
+        "poster_path": "/dBQuk2LkHjrDsSjueirPQg96GCc.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            10749,
+            18,
+            27
+        ],
+        "popularity": 357.541,
+        "release_date": "2022-11-18",
+        "video": false,
+        "vote_average": 7.518,
+        "vote_count": 388
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/98bqsUBVjXpbUt1fRkjn7hJQXrq.jpg",
+        "id": 157080,
+        "name": "Treason",
+        "original_language": "en",
+        "original_name": "Treason",
+        "overview": "An MI6 deputy's bright future takes a sharp turn after a reunion with a Russian spy forces him to question his entire life.",
+        "poster_path": "/qOitarv5B5ydsScN3dkJPipYPg3.jpg",
+        "media_type": "tv",
+        "genre_ids": [
+            18,
+            10759
+        ],
+        "popularity": 47.404,
+        "first_air_date": "2022-12-26",
+        "vote_average": 7,
+        "vote_count": 1,
+        "origin_country": [
+            "GB"
+        ]
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/mNHRGO1gFpR2CYZdANe72kcKq7G.jpg",
+        "id": 153312,
+        "name": "Tulsa King",
+        "original_language": "en",
+        "original_name": "Tulsa King",
+        "overview": "Just after he is released from prison after 25 years, New York mafia capo Dwight “The General” Manfredi is unceremoniously exiled by his boss to set up shop in Tulsa, Okla. Realizing that his mob family may not have his best interests in mind, Dwight slowly builds a “crew” from a group of unlikely characters, to help him establish a new criminal empire in a place that to him might as well be another planet.",
+        "poster_path": "/fwTv3RPRAIy0maOMns5eYRRwnDk.jpg",
+        "media_type": "tv",
+        "genre_ids": [
+            80,
+            18
+        ],
+        "popularity": 447.857,
+        "first_air_date": "2022-11-13",
+        "vote_average": 8.81,
+        "vote_count": 84,
+        "origin_country": [
+            "US"
+        ]
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/vNuHqmOJRQXY0PBd887DklSDlBP.jpg",
+        "id": 315162,
+        "title": "Puss in Boots: The Last Wish",
+        "original_language": "en",
+        "original_title": "Puss in Boots: The Last Wish",
+        "overview": "Puss in Boots discovers that his passion for adventure has taken its toll: He has burned through eight of his nine lives, leaving him with only one life left. Puss sets out on an epic journey to find the mythical Last Wish and restore his nine lives.",
+        "poster_path": "/lmf0zzR7ritjOL3qumRh3hfvOFK.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            16,
+            12,
+            35,
+            10751,
+            14
+        ],
+        "popularity": 1145.968,
+        "release_date": "2022-12-07",
+        "video": false,
+        "vote_average": 7.975,
+        "vote_count": 99
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/e782pDRAlu4BG0ahd777n8zfPzZ.jpg",
+        "id": 555604,
+        "title": "Guillermo del Toro's Pinocchio",
+        "original_language": "en",
+        "original_title": "Guillermo del Toro's Pinocchio",
+        "overview": "During the rise of fascism in Mussolini's Italy, a wooden boy brought magically to life struggles to live up to his father's expectations.",
+        "poster_path": "/vx1u0uwxdlhV2MUzj4VlcMB0N6m.jpg",
+        "media_type": "movie",
+        "genre_ids": [
+            16,
+            14,
+            18
+        ],
+        "popularity": 2741.388,
+        "release_date": "2022-11-09",
+        "video": false,
+        "vote_average": 8.469,
+        "vote_count": 1053
+    }
+];
+
+export default movies;
